ea763f4a6b7b88bdae2c35b197fd57c9b5cb114d
martijnvg <martijn.11@live.nl>
2024-11-03 23:58:06 +0100

    initial push

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstddef>
+
+#include <sqlite3.h>
+
+int main() 
+{
+    
+    // std::byte s{"test"};
+    std::cout << "Test" << std::endl;
+    std::cout << "Test2" << std::endl;
+    // std::cout << s << std::endl;
+    return 0;
+}
